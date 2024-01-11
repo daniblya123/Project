06be9cf91c2f1e300689f47c70d069fa906afb4c
daniblya123 <144435730+daniblya123@users.noreply.github.com>
2024-01-11 10:31:35 +0000

    yes

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,52 @@
-function Login() {
-  return <p>Login</p>;
+import { FormEvent, useState } from "react";
+import axios from "axios"
+
+function Login(){
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [responseText, setResponseText] = useState("");
+  
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setResponseText("");
+    try {
+      const response = await axios.post("https://localhost:5000/login", {
+        username: email,
+        password: pass,
+      });
+      setResponseText(response.data);
+      console.log(response.data)
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setResponseText(error.message);
+      } else {
+        setResponseText(String(error));
+      }
+    }
+  }
+  const getQuote = () => {
+    axios.get('https://api.quotable.io/random')
+    .then(res => {
+      console.log(res.data.content)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  return (
+  <>
+  <div className="LoginForm">
+      <form onSubmit={handleSubmit}>
+          <label for="email">Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+          <label for="password">Password</label>
+          <input value={pass} onChange={(e) => setPass(e. target.value)} type="password" placeholder="*********" id="password" name="password"/>
+          <button type="submit">Login</button>
+      </form>
+  </div>
+  <button type="button" onClick={getQuote}>Click me</button>
+  </>
+  )
 }
 
 export default Login;
---
+++ src/flask-server.py
@@ -0,0 +1,16 @@
+from bcrypt import checkpw, gensalt, hashpw
+from flask import Flask, request
+from flask_cors import CORS
+  
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/login", methods=["POST"])
+
+def login():
+    username = request.json["username"]
+    providedPassword = request.json["password"].encode("utf-8")
+    if username == "dani":
+        return "Valid login.", 200
+    
+    else: return "Invalid login.", 200
